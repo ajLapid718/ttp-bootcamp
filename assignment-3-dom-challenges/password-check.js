1ea5eff045a5d3f0658d57b38c9d1a9a8097a7e8
unknown <tfung5@gmail.com>
2019-05-30 22:53:49 -0400

    Separated files

---
+++ assignment-3-dom-challenges/password-check.js
@@ -0,0 +1,33 @@
+let form__username = document.getElementById("form__username");
+let form__email = document.getElementById("form__email");
+let form__password = document.getElementById("form__password");
+let form__submit = document.getElementById("form__submit");
+
+form__submit.addEventListener("click", function(event) {
+  let form__username__value = "";
+  if (form__username) {
+    form__username__value = form__username.value;
+  }
+
+  let form__email__value = "";
+  if (form__email) {
+    form__email__value = form__email.value;
+  }
+
+  let form__password__value = "";
+  if (form__password) {
+    form__password__value = form__password.value;
+  }
+
+  if (form__password.value.match(/\b12345678\b/)) {
+    document.getElementById("form__check__h1").innerHTML =
+      "Username: " +
+      form__username__value +
+      "<br>Email: " +
+      form__email__value +
+      "<br>Password: " +
+      form__password__value;
+  } else {
+    alert("Password is NOT correct!");
+  }
+});
