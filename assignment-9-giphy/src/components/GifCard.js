fda7b49186d737ab9937d8a42eb6e95177e351d9
unknown <tfung5@gmail.com>
2019-06-07 23:10:20 -0400

    Modified giphy GifCard to use props directly

--- assignment-9-giphy/src/components/GifCard.js
+++ assignment-9-giphy/src/components/GifCard.js
@@ -1,34 +1,15 @@
 import React, { Component } from "react";
-import axios from "axios";
 import PropTypes from "prop-types";
-import {
-  Container,
-  Col,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Button
-} from "reactstrap";
 
 export default class GifCard extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: []
-    };
   }
-
-  componentWillReceiveProps = nextProps => {
-    this.setState({
-      data: nextProps.data
-    });
-  };
 
   displayGifs = () => {
     return (
       <div className="gifs">
-        {this.state.data.map((gif, index) => (
+        {this.props.data.map((gif, index) => (
           <div className="gif" key={index}>
             <img src={gif.images.fixed_width.url} />
           </div>
